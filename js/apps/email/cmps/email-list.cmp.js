ea5fbb3a64000b48d4b84b0446b12ab0485480cf
kerensoko86 <keren.sokoletsky@gmail.com>
2020-02-26 19:17:24 +0200

    save

--- js/apps/email/cmps/email-list.cmp.js
+++ js/apps/email/cmps/email-list.cmp.js
@@ -5,13 +5,13 @@ export default {
     name: 'email-list',
     template: `
     <section class="emails-container">
-     <!-- <router-link v-for="(email, idx) in emails" :to="'email/'+email.id" exact> -->
+     <router-link v-for="(email, idx) in emails" :to="'email/'+email.id" exact>
         <div class="email-preview" v-for="(email, idx) in emails">
             <email-preview :key="idx" :email="email">
                 <email-details></email-details>
             </email-preview>
         </div>
-    <!-- </router-link> -->
+    </router-link>
  </section>
     `,
     props: ['emails'],
@@ -32,11 +32,16 @@ export default {
     },
     components: {
         emailPreview,
-<<<<<<< HEAD
-        emailDetails
-=======
+        <<
+        <<
+        << < HEAD
+        emailDetails ===
+        ===
+        =
         emailDetails,
         // emailCompose
->>>>>>> f60ac8b655588acc9b194ae0d7d1389b9af4cc0b
+        >>>
+        >>>
+        > f60ac8b655588acc9b194ae0d7d1389b9af4cc0b
     }
 }
