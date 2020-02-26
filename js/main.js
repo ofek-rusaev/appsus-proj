e4fb29c28e8ceadc3a2eb8a34475681c8eda0640
kerensoko86 <keren.sokoletsky@gmail.com>
2020-02-26 12:23:53 +0200

    nav bar

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import routes from './routes.js'
-// import appHeader from './cmps/app-header.cmp.js'
+import appHeader from './apps/email/cmps/app-header.cmp.js'
 import emailApp from './apps/email/cmps/email-app.cmp.js'
 // import userMsg from './cmps/user-msg.cmp.js'
 
@@ -8,17 +8,17 @@ const router = new VueRouter({ routes })
 new Vue({
     el: '#app',
     router,
-        template: `
+    template: `
         <section class="email-app">
             <h1>Email App</h1>
-            <!-- <app-header></app-header> -->
+            <app-header></app-header>
             <!-- <user-msg></user-msg> -->
             <router-view></router-view>
         </section>
     `,
-        components: {
-            emailApp,
-            // appHeader,
-            // userMsg
-        }
+    components: {
+        emailApp,
+        appHeader,
+        // userMsg
+    }
 })
