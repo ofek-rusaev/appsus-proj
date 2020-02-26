4061ec0b0ce265f833d402b0fec658d413844cf6
kerensoko86 <keren.sokoletsky@gmail.com>
2020-02-26 19:15:53 +0200

    save

--- js/routes.js
+++ js/routes.js
@@ -7,21 +7,9 @@ import emailCompose from './apps/email/cmps/email-compose.cmp.js'
 import emailList from './apps/email/cmps/email-list.cmp.js'
 
 
-<<<<<<< HEAD
 const routes = [
     { path: '/email', component: emailApp },
     // { path: '/email/:id', component: emailDetails },
-=======
-const routes = [{
-        path: '/email',
-        component: emailApp,
-        children: [
-            { path: '', component: emailList },
-            { path: 'compose', component: emailCompose }
-        ]
-    },
-    { path: '/email/:id', component: emailDetails },
->>>>>>> 1d8c59158c43d59b0b6d16d9b00e4c6ddbb17d72
     { path: '/note', component: noteApp },
     { path: '/note/:id', component: noteDetails },
 ];
