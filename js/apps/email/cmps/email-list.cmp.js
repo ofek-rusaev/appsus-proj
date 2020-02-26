5a55563fb7007a7de359ee92a138c55021b4a9f1
kerensoko86 <keren.sokoletsky@gmail.com>
2020-02-26 16:48:31 +0200

    save

--- js/apps/email/cmps/email-list.cmp.js
+++ js/apps/email/cmps/email-list.cmp.js
@@ -3,10 +3,19 @@ import emailPreview from './email-preview.cmp.js'
 export default {
     name: 'email-list',
     template: `
+    <section class="email-bar">
+    <input type="search" v-model
+         <section class="left-bar">
+             <div>Inbox</div>
+             <div>Starred</div>
+             <div>Sent Mail</div>
+             <div>Drafts</div>
+    </section>
     <section class="emails-container">
-     <router-link v-for="(email, idx) in emails" :to="'email/'+email.id" exact>
+    <router-link v-for="(email, idx) in emails" :to="'email/'+email.id" exact>
         <div class="email-preview"><email-preview :key="idx" :email="email"></email-preview></div>
     </router-link>
+    </section>
  </section>
     `,
     props: ['emails'],
