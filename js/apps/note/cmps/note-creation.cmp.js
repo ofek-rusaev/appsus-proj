4f1eb795d9043476af6fc5bb9f68750f43749df0
ofek-rusaev <ofek2602@gmail.com>
2020-02-28 14:02:28 +0200

    adding note

--- js/apps/note/cmps/note-creation.cmp.js
+++ js/apps/note/cmps/note-creation.cmp.js
@@ -1,65 +1,55 @@
-var inputText = {
-    template: `
-        <input type="text" 
-            :placeholder="info.placeholder" 
-            v-model="txt" 
-            @change="$emit('changed', txt)" />
-    `,
-    props: ['info'],
-    data() {
-        return {
-            txt: ''
-        }
-    }
-}
-
-const noteCmps = [
-    {
-        name:'noteInputText',
-        type:'text',
-        info: {
-            placeholder : "Add text"
-        }
-    },
-    {
-        name:'noteInputImg',
-        type:'text',
-        info: {
-            placeholder : "Add image url"
-        }
-    },
-    {
-        name:'noteInputTotos',
-        type:'text',
-        info: {
-            placeholder : "Separate by comma"
-        }
-    },
-]
+import {noteService} from '../services/note.service.js';
 
 export default {
     template: `
         <section class="note-add">
-            <input type="text" />            
-                <input type="radio" id="text" name="text" v-model="type" value="other">
+            <input type="text" :placeholder="updatePlaceholder" @keyup.enter="addNote"/>            
+                <input type="radio" id="text" name="text" v-model="type" value="noteText">
                 <label for="text">Text</label><br>
-                <input type="radio" id="img" name="text" v-model="type" value="other">
+                <input type="radio" id="img" name="text" v-model="type" value="noteImg">
                 <label for="img">Image</label><br>
-                <input type="radio" id="todo" name="text" v-model="type" value="other">
+                <input type="radio" id="todo" name="text" v-model="type" value="noteTodo">
                 <label for="todo">Todo</label>
-                <input type="radio" id="video" name="text" v-model="type" value="other">
+                <input type="radio" id="video" name="text" v-model="type" value="noteVid">
                 <label for="video">Video</label>
+                <pre>{{note}}</pre>
         </section>
     `,
     data() {
         return {
-            type: ''
+            type: 'noteText',
+            note: {}
         }
     },
-    created() {
-        // this.results = new Array(this.cmps.length)
+    computed: {
+        updatePlaceholder() {
+            if (this.type === 'noteText') {
+                return 'What\'s on your mind...'
+            }
+            if (this.type === 'noteImg') {
+                return 'Add image url'
+            }
+            if (this.type === 'noteTodo') {
+                return 'Separate by comma...'
+            }
+            if (this.type === 'noteVid') {
+                return 'Add video url'
+            }
+
+        }
+
     },
-    components: {
-        inputText
+    methods: {
+        addNote() {
+                noteService.saveNote(this.note)
+                    .then(note => {
+                        this.note = noteService.getEmptyNote()
+                        return this.note;
+                    })
+                // this.$router.push('/email/inbox')
+            console.log('adding note');
+            console.log(this.note);
+            
+        }
     }
 }
