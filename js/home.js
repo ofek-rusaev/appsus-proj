bfe5539ae580e15bb60d5e6fce23b76c8c0c079d
ofek-rusaev <ofek2602@gmail.com>
2020-03-02 11:22:07 +0200

    removed headbar duplication

--- js/home.js
+++ js/home.js
@@ -3,11 +3,10 @@ import appHeader from './general-cmps/app-header.cmp.js';
 export default {
     template: `
     <section >
-    <app-header></app-header>
-
-    <img class="main-pic" src="img/bcg.jfif"/>
-            <h1>Welcome To The Future. Appsus.</h1>
-         </section>
+        <!-- <app-header></app-header> -->
+        <h1>Welcome To The Future. Appsus.</h1>
+        <img class="main-pic" src="img/bcg.jfif"/>
+    </section>
 `,
     components: {
         appHeader
